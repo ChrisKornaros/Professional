f0b2414df15c9fa3b8da60221ea67fb2b0c3b90b
chriskornaros <chriskornaros@gmail.com>
2024-11-11 16:20:09 -0600

    Basic DuckDB instance. Setup a persistent database for Kaggle's Titanic datasets (both test and train). Took about 20 minutes total, impressed with the ease of use.

---
+++ Projects/DuckDB_Introduction/scripts/basic.sql
@@ -0,0 +1,21 @@
+-- Setting up a basic table using a SQL script
+-- This is the train.csv dataset for the Kaggle Titanic competition
+CREATE TABLE train (
+    PassengerID VARCHAR,
+    Survived VARCHAR,
+    Pclass VARCHAR,
+    Name VARCHAR,
+    Sex VARCHAR,
+    Age VARCHAR,
+    SibSp VARCHAR,
+    Parch VARCHAR,
+    Ticket VARCHAR,
+    Fare VARCHAR,
+    Cabin VARCHAR,
+    Embarked VARCHAR
+);
+COPY train FROM 'data/train.csv'; -- Using a very basic way
+
+-- Creating the test dataset for the Kaggle Titanic competition, using duckdb features
+CREATE TABLE test AS
+    SELECT * FROM 'data/test.csv';
